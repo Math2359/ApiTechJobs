1dbbed9263dee63c934b8c93399de1f1e931dbb9
Mateus Ruiz <mmf202014@gmail.com>
2025-11-01 11:55:02 -0300

    Update project for .NET 8.0 and AWS Lambda support
    - Target framework updated to .NET 8.0 in `Api.csproj`.
    - Added `<AwsProjectType>` property and new package reference for `Amazon.Lambda.AspNetCoreServer.Hosting` version `1.9.0`.
    - Modified `Program.cs` to support AWS Lambda hosting with `AddAWSLambdaHosting`.
    - Introduced `AdicionarSwagger()` method and removed `RegistrarAutenticaco()`.
    - Simplified Swagger middleware setup to always use it, removing the development environment check.

--- Api/Program.cs
+++ Api/Program.cs
@@ -6,24 +6,21 @@ builder.Configuration.AddEnvironmentVariables();
 
 builder.Services.AddControllers();
 
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
+
 builder.Services.AdicionarSwagger();
 
 builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.ConfigurarServicos();
-
 builder.RegistrarAutenticaco();
 
 builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
